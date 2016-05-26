0e7931e53fb9550efb1146b7e63fc8a0cbf9224d
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-26 19:27:37 +0300

    Set up the ArrayList of Crime objects

--- android/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -2,12 +2,18 @@ package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class CrimeLab {
+    private ArrayList<Crime> mCrimes;
+
     private static CrimeLab sCrimeLab;
     private Context mAppContext;
 
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
+        mCrimes = new ArrayList<>();
     }
 
     public static CrimeLab getInstance(Context c) {
@@ -15,5 +21,17 @@ public class CrimeLab {
             sCrimeLab = new CrimeLab(c.getApplicationContext());
         }
         return sCrimeLab;
+    }
+
+    public ArrayList<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id))
+                return crime;
+        }
+        return null;
     }
 }
