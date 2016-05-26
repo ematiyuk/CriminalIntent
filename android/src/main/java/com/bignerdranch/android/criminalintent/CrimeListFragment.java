41346ca5dec1e7c9c777d43b057c81188a3442bf
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-26 20:03:01 +0300

    Create the CrimeListFragment
    - set the title of the fragment's host activity
    - add string resource for the new activity title
    - access crimes from CrimeLab singleton

---
+++ android/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+
+import java.util.ArrayList;
+
+public class CrimeListFragment extends ListFragment {
+    private ArrayList<Crime> mCrimes;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /* set the title of the fragment's host activity;
+           this title will be displayed on the action bar (or title bar on older devices) */
+        getActivity().setTitle(R.string.crimes_title);
+
+        /* get the CrimeLab singleton and then get the list of crimes */
+        mCrimes = CrimeLab.getInstance(getActivity()).getCrimes();
+    }
+}
