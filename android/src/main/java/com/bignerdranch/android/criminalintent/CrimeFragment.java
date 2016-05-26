f7a645b8003af88286e38689c6a2aee041501478
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-26 16:58:44 +0300

    Update(CrimeFragment): wire widgets

--- android/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -7,11 +7,16 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,20 @@ public class CrimeFragment extends Fragment {
                 // this one too
             }
         });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false); // disable for now; it will be enabled later
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                // set the crime's solved property
+                mCrime.setSolved(isChecked);
+            }
+        });
+
         return v;
     }
 }
