653e5f93205e93ca9b9c443dfe1b53cfb4621dd3
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-26 16:31:38 +0300

    Upgrade Crime
    - add the date a crime occurred
    - add a flag that represents whether the crime has been solved
    - add getters and setters for them
    - uodate constructor contents

--- android/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,14 +1,19 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
     private String mTitle;
+    /** represents the date a crime occurred */
+    private Date mDate;
+    /** represents whether the crime has been solved */
+    private boolean mSolved;
 
     public Crime() {
-        // generate unique identifier
-        mId = UUID.randomUUID();
+        mId = UUID.randomUUID(); // generate unique identifier
+        mDate = new Date(); // sets mDate to the current date (the default date for a crime)
     }
 
     public UUID getId() {
@@ -21,5 +26,21 @@ public class Crime {
 
     public void setTitle(String title) {
         this.mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setDate(Date date) {
+        this.mDate = date;
+    }
+
+    public void setSolved(boolean solved) {
+        this.mSolved = solved;
     }
 }
