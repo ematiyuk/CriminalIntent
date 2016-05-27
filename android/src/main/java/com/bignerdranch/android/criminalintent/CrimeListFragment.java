0ea473cd75cea311bb966ba6ebe318026fb83041
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-27 14:58:23 +0300

    Start CrimeActivity from CrimeListFragment

--- android/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
@@ -34,7 +35,10 @@ public class CrimeListFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         Crime crime = ((CrimeAdapter) getListAdapter()).getItem(position);
-        Log.d(TAG, crime.getTitle() + " was clicked");
+
+        // start CrimeActivity
+        Intent intent = new Intent(getActivity(), CrimeActivity.class);
+        startActivity(intent);
     }
 
     private class CrimeAdapter extends ArrayAdapter<Crime> {
