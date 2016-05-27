48e219eab775c1091a4a4b846f56016b81972c16
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-27 14:24:25 +0300

    Override adapter's getView()

--- android/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -41,6 +44,27 @@ public class CrimeListFragment extends ListFragment {
 
         public CrimeAdapter(ArrayList<Crime> crimes) {
             super(getActivity(), 0, crimes);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // If we weren't given a view, inflate one
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.list_item_crime, null);
+            }
+
+            // configure the view for this crime
+            Crime crime = getItem(position);
+
+            TextView titleTextView = (TextView) convertView.findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(crime.getTitle());
+            TextView dateTextView = (TextView) convertView.findViewById(R.id.crime_list_item_dateTextView);
+            dateTextView.setText(crime.getDate().toString());
+            CheckBox solvedCheckBox = (CheckBox) convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            solvedCheckBox.setChecked(crime.isSolved());
+
+            return convertView;
         }
     }
 }
