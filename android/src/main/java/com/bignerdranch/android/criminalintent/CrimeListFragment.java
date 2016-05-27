8370072c5d25d1f13fa266a13bff41c466c42c77
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-27 14:34:35 +0300

    Wire up the CrimeAdapter to CrimeListFragment

--- android/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -26,17 +26,13 @@ public class CrimeListFragment extends ListFragment {
         /* get the CrimeLab singleton and then get the list of crimes */
         mCrimes = CrimeLab.getInstance(getActivity()).getCrimes();
 
-        ArrayAdapter<Crime> adapter =
-                new ArrayAdapter<Crime>(getActivity(),
-                                        android.R.layout.simple_list_item_1,
-                                        mCrimes);
-
+        CrimeAdapter adapter = new CrimeAdapter(mCrimes);
         setListAdapter(adapter);
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        Crime crime = (Crime) getListAdapter().getItem(position);
+        Crime crime = ((CrimeAdapter) getListAdapter()).getItem(position);
         Log.d(TAG, crime.getTitle() + " was clicked");
     }
 
