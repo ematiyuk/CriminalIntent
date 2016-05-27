c7eddda55b1aae7ef660b1a4832095e0eef4fb3d
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-27 18:48:08 +0300

    Set up pager adapter

--- android/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -1,11 +1,17 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
 
 public class CrimePagerActivity extends FragmentActivity {
     private ViewPager mViewPager;
+    private ArrayList<Crime> mCrimes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,21 @@ public class CrimePagerActivity extends FragmentActivity {
         mViewPager = new ViewPager(this);
         mViewPager.setId(R.id.viewPager);
         setContentView(mViewPager);
+
+        mCrimes = CrimeLab.getInstance(this).getCrimes();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
 }
