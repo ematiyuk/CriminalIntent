c8fe432745a46188af07ed828fc4fc1e47e77640
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-28 16:27:57 +0300

    Extract date and initialize DatePicker

--- android/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -5,8 +5,11 @@ import android.app.Dialog;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DatePickerFragment extends DialogFragment {
     public static final String EXTRA_DATE = "com.bignerdranch.android.criminalintent.date";
@@ -15,7 +18,28 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mDate = (Date) getArguments().getSerializable(EXTRA_DATE);
+
+        // create a Calendar to get the year, month and day
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+
+        DatePicker datePicker = (DatePicker) view.findViewById(R.id.dialog_date_datePicker);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+                // translate year, month, day into a Date object using a calendar
+                mDate = new GregorianCalendar(year, month, day).getTime();
+
+                // update argument to preserve selected value on rotation
+                getArguments().putSerializable(EXTRA_DATE, mDate);
+            }
+        });
 
         return new AlertDialog.Builder(getActivity())
                 .setView(view)
