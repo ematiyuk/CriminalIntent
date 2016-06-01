f0b1754aaac08843448e5b937ca4de60ff97d370
Eugene Matiyuk <ematiyuk@gmail.com>
2016-06-01 23:38:49 +0300

    Add newCrime method to model

--- android/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -31,6 +31,10 @@ public class CrimeLab {
         return sCrimeLab;
     }
 
+    public void addCrime(Crime crime) {
+        mCrimes.add(crime);
+    }
+
     public ArrayList<Crime> getCrimes() {
         return mCrimes;
     }
