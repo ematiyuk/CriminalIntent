47709329cd40821475998ea8a1ba01dd6d844d12
Eugene Matiyuk <ematiyuk@gmail.com>
2016-06-08 16:10:29 +0300

    Save changes persistently in CrimeLab

--- android/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -1,12 +1,18 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class CrimeLab {
+    private static final String TAG = "CrimeLAb";
+    private static final String FILENAME = "crimes.json";
+
     private ArrayList<Crime> mCrimes;
+    private CriminalIntentJSONSerializer mSerializer;
 
     private static CrimeLab sCrimeLab;
     private Context mAppContext;
@@ -14,6 +20,7 @@ public class CrimeLab {
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
         mCrimes = new ArrayList<>();
+        mSerializer = new CriminalIntentJSONSerializer(mAppContext, FILENAME);
     }
 
     public static CrimeLab getInstance(Context c) {
@@ -25,6 +32,18 @@ public class CrimeLab {
 
     public void addCrime(Crime crime) {
         mCrimes.add(crime);
+    }
+
+    public boolean saveCrimes() {
+        try {
+            mSerializer.saveCrimes(mCrimes);
+            Log.d(TAG, "crimes saved to file");
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "Error saving crimes: ", e);
+            Toast.makeText(mAppContext, R.string.error_saving_crimes_msg, Toast.LENGTH_LONG).show();
+            return false;
+        }
     }
 
     public ArrayList<Crime> getCrimes() {
