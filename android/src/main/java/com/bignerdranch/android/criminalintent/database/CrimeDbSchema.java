78ba5489edefaad4d160a8ae9d5fd09cc32ed7cf
Eugene Matiyuk <ematiyuk@gmail.com>
2016-06-16 17:17:13 +0300

    Define CrimeTable in CrimeDbSchema

---
+++ android/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.criminalintent.database;
+
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+    }
+}
