d1fd8d725cd6176d4eddb3ba91d0fc51d4a0c661
Eugene Matiyuk <ematiyuk@gmail.com>
2016-06-20 16:17:21 +0300

    Add suspect field to crime database

--- android/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.bignerdranch.android.criminalintent.Crime;
 import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 public class CrimeBaseHelper extends SQLiteOpenHelper {
@@ -22,7 +23,8 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
                 CrimeTable.Cols.UUID + ", " +
                 CrimeTable.Cols.TITLE + ", " +
                 CrimeTable.Cols.DATE + ", " +
-                CrimeTable.Cols.SOLVED +
+                CrimeTable.Cols.SOLVED + ", " +
+                CrimeTable.Cols.SUSPECT +
                 ")"
         );
     }
