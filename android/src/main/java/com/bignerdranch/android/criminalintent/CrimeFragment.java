a6d87972b42141f914cd931d96d8f0176a15f101
Eugene Matiyuk <ematiyuk@gmail.com>
2016-06-22 00:00:06 +0300

    Create report intent with ShareCompat.IntentBuilder

--- android/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -14,6 +14,7 @@ import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.NavUtils;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -191,11 +192,12 @@ public class CrimeFragment extends Fragment {
         mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
-                intent.putExtra(Intent.EXTRA_SUBJECT,
-                        getString(R.string.crime_report_subject, getString(R.string.app_name)));
+                Intent intent = ShareCompat.IntentBuilder.from(getActivity())
+                        .setText(getCrimeReport())
+                        .setSubject(getString(R.string.crime_report_subject,
+                                getString(R.string.app_name)))
+                        .setType("text/plain")
+                        .getIntent();
                 intent = Intent.createChooser(intent, getString(R.string.send_report));
                 startActivity(intent);
             }
