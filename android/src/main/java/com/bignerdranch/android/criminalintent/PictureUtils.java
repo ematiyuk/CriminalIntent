b7ea7c13071ecbd322a9bd61d928110a0bbf68a4
Eugene Matiyuk <ematiyuk@gmail.com>
2016-06-28 23:59:56 +0300

    Add a method to clean up ImageView

--- android/src/main/java/com/bignerdranch/android/criminalintent/PictureUtils.java
+++ android/src/main/java/com/bignerdranch/android/criminalintent/PictureUtils.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.widget.ImageView;
 
 public class PictureUtils {
     public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight) {
@@ -37,5 +39,15 @@ public class PictureUtils {
         activity.getWindowManager().getDefaultDisplay().getSize(size);
 
         return getScaledBitmap(path, size.x, size.y);
+    }
+
+    public static void cleanImageView(ImageView imageView) {
+        if (!(imageView.getDrawable() instanceof BitmapDrawable))
+            return;
+
+        // clean up the view's image for the sake of memory
+        BitmapDrawable b = (BitmapDrawable)imageView.getDrawable();
+        b.getBitmap().recycle();
+        imageView.setImageDrawable(null);
     }
 }
